8bd00701bfd61dff5ad37aa0b9371cac6d6adb02
Anton Dolgopolik <antondolgopolikv@gmail.com>
2022-04-14 05:03:19 +0300

    Add base structure

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import {Component} from "react";
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {apiResponse: ""};
+    }
+
+    callAPI() {
+        fetch("http://localhost:9000/users")
+            .then(res => res.text())
+            .then(res => this.setState({apiResponse: res}));
+    }
+
+    componentWillMount() {
+        this.callAPI();
+    }
+
+    render() {
+        return (<div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <p className="App-intro">{this.state.apiResponse}</p>
+            </header>
+        </div>);
+    }
+}
+
+export default App;
